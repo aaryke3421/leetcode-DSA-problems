017092370080b139a3f47dd70549c9c80734c619
AARYA KESHARI <aaryakeshari9569@gmail.com>
2026-01-25 02:38:40 +0530

    Time: 1 ms (76.32%), Space: 46.4 MB (93.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,21 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Agar array khali hai toh length 0 return karein
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        int i = 0; // Pehla element hamesha unique hota hai
-        
-        for (int j = 1; j < nums.length; j++) {
-            // Agar current element pichle unique element se alag hai
-            if (nums[j] != nums[i]) {
-                i++;           // Unique spot ko aage badhao
-                nums[i] = nums[j]; // Naya unique element wahan rakh do
+        if(nums.length==0)return 0;
+        int officer=1;
+        int unique=1;
+        int cm=1;
+        while(cm<nums.length){
+            if(nums[cm]==nums[cm-1]){
+                cm++;
+                continue;
             }
+            nums[officer]=nums[cm];
+            officer++;
+            cm++;
+            unique++;
         }
+        return unique;
         
-        // i index hai, length return karne ke liye +1 karein
-        return i + 1;
     }
 }
